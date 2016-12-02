b9b973f2552bc1f3a6d21c75c5f755e877217a91
user <user@FE05>
2016-12-02 08:56:30 +0800

    set array

---
+++ src/array/Main.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// primitive type array
+		int[] nArray = new int[5];
+
+		for (int i = 0; i < nArray.length; i++) {
+			System.out.println(nArray[i]);
+		}
+
+		// class type array
+		String[] sArray = new String[5];
+
+		for (int i = 0; i < sArray.length; i++) {
+			System.out.println(sArray[i]);
+		}
+
+	}
+
+}
