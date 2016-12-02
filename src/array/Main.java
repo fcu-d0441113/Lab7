82ba24d83103d698ab95cca2596287afa909bd75
user <user@FE05>
2016-12-02 09:26:33 +0800

    array for-each

--- src/array/Main.java
+++ src/array/Main.java
@@ -5,15 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 
 		int[] nArray = { 1, 3, 5, 7 };
-		for (int i = 0; i < nArray.length; i++) {
-			System.out.println(nArray[i]);
+		for (int aElm : nArray) {
+			System.out.println(aElm);
 		}
 
-		String[] sArray = { "John", "Tom", "May" };
-		for (int i = 0; i < sArray.length; i++) {
-			System.out.println(sArray[i]);
+		String[] sArray = { "John", "Tom", "Mau" };
+		for (String aElm : sArray) {
+			System.out.println(aElm);
 		}
-
 	}
 
 }
